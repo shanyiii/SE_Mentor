3eb529c0d2503bd93aed2891b2d8e7b6a0ff144a
shanyiii <syncduplicate@gmail.com>
2026-07-15 15:53:05 +0800

    config example

---
+++ config_example.py
@@ -0,0 +1,9 @@
+OPENAI_API_KEY = "<openai-api-key>"
+GEMINI_API_KEY = "<gemini-api-key>"
+CLAUDE_API_KEY = "<claude-api-key>"
+
+LANGCHAIN_API_KEY = "<langchain-api-key>"
+
+DISCORD_TOKEN = "<discord-bot-token>"
+
+NEO4J_PASSWORD = "<your-neo4j-password>"
